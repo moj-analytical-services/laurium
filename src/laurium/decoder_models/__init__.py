717c27ff255c9074807a5b43fb5afe75e37b2edc
Mike Ratford <mike.ratford@googlemail.com>
2026-06-26 16:12:29 +0000

    Add some unused functions to test the coverage action.

--- src/laurium/decoder_models/__init__.py
+++ src/laurium/decoder_models/__init__.py
@@ -1,6 +1,6 @@
 """Top level API for lauriums LLM-based extraction sub-module."""
 
-from laurium.decoder_models import prompts
+from laurium.decoder_models import prompts, qa_nonsense
 from laurium.decoder_models.extract import BatchExtractor
 from laurium.decoder_models.llm import create_llm
 
